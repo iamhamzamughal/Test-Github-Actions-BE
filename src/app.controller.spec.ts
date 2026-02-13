916300b5d0cfbaedd58cf246a79896fa539bcb85
Hamza <hamzamughal00@gmail.com>
2026-02-13 17:16:40 +0500

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World  How you doing!"', () => {
+      expect(appController.getHello()).toBe('Hello World  How you doing!');
     });
   });
 });
